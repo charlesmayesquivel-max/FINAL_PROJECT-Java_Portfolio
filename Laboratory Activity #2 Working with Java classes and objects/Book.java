6d4c265ef61b3b5be4d2ccc4b18a91b772006f17
Charles May T. Esquivel <charlesmay.esquivel@cvsu.edu.ph>
2026-01-19 22:51:31 +0800

    A

---
+++ Laboratory Activity #2 Working with Java classes and objects/Book.java
@@ -0,0 +1,34 @@
+public class Book {
+    private final String title;
+    private String author;
+    private final int yearPublished;
+    private int currentPage;
+
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.currentPage = 0;
+    }
+
+    public void displayDetails() {
+        System.out.println("Title: " + title + ", Author: " + author +
+                           ", Year: " + yearPublished + ", Current Page: " + currentPage);
+    }
+
+    public void changePage(int page) {
+        this.currentPage = page;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
